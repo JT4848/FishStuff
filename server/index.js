71304f727d26b7bda6bf203826000fd7e8db616d
JT4848 <jtpricone@gmail.com>
2023-10-16 03:12:53 -0400

    scaffolding done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express();
-const PORT = 3001;
+const PORT = 6969;
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "..", "client/dist")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client/dist/index.html"));
+});
 
 
 app.use("/api", require("./api"));
